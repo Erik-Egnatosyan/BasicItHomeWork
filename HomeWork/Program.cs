6a83af38a31d411408795b5ff3f822e218a3cacf
Erik Egnatosyan <egnatosyan.sans@mail.ru>
2023-02-16 17:33:28 +0400

    add to 57

--- HomeWork/Program.cs
+++ HomeWork/Program.cs
@@ -34,6 +34,17 @@
             Console.Write("  #34\n");
             IsTwoNumberIsOdd36();
             Console.Write("  #35\n");
+            FirstTwoEqaulThird51();
+            Console.Write("  #51\n");
+            IsOneOfThisSame52();
+            Console.Write("  #52\n");
+            CalculateDivForNumber53();
+            Console.Write("  #53\n");
+            CalcMaxNumber54();
+            Console.Write("  #54\n");
+            CalcMinNumber55();
+            Console.Write("  #55\n");
+            Something56();
         }
         public static void MaxNumber21()
         {
@@ -74,14 +85,14 @@
             int a = 0;
             int b = 0;
             int c = 1;
-            Console.Write(a == 1 || b == 1 || c == 1 ? true : false); 
+            Console.Write(a == 1 || b == 1 || c == 1 ? true : false);
         }
         public static void IfTwoNumberIsTwo24()
         {
             int a = 0;
             int b = 2;
             int c = 2;
-            Console.Write((a == 2 && b == 2) || (a == 2 && c == 2) || (b == 2 && c ==2) ? true : false);
+            Console.Write((a == 2 && b == 2) || (a == 2 && c == 2) || (b == 2 && c == 2) ? true : false);
         }
         public static void TriangleSolve25()
         {
@@ -113,15 +124,15 @@
             int c = 60;
             if (a > b && a > c && b > c)
                 Console.Write($"{a},{b},{c}");
-            else if(a > c && a > b && c > b)
+            else if (a > c && a > b && c > b)
                 Console.Write($"{a},{c},{b}");
-            else if(b > c && b > a && c > a)
+            else if (b > c && b > a && c > a)
                 Console.Write($"{b},{c},{a}");
-            else if(b > a && b > c && a > c)
+            else if (b > a && b > c && a > c)
                 Console.Write($"{b},{a},{c}");
             else if (c > a && c > b && a > b)
                 Console.Write($"{c},{a},{b}");
-            else 
+            else
                 Console.Write($"{c},{b},{a}");
         }
         public static void ToTheDown30()
@@ -160,7 +171,7 @@
             }
             if (b > c && b > d)
                 Console.Write("b is the largest!");
-            else if(c > d)
+            else if (c > d)
                 Console.Write("c is the largest!");
         }
         public static void MinNumber4digit32()
@@ -215,6 +226,78 @@
             int c = 4;
             int d = 2;
             Console.Write(((a % 2 == 1) || (b % 2 == 1) || (c % 2 == 1) || (d % 2 == 1)) ? true : false);
+        }
+        public static void FirstTwoEqaulThird51()
+        {
+            int t = 347;
+            int a = t / 100;
+            int b = (t / 10) % 10;
+            int c = t % 10;
+            Console.Write(a + b == c ? true : false);
+        }
+        public static void IsOneOfThisSame52()
+        {
+            int t = 343;
+            int a = t / 100;
+            int b = (t / 10) % 10;
+            int c = t % 10;
+            Console.Write((a == b || a == c || c == b) ? true : false);
+        }
+        public static void CalculateDivForNumber53()
+        {
+            int t = 343;
+            int k = 520;
+            int a = t / 100;
+            int b = (t / 10) % 10;
+            int c = t % 10;
+            int result = a + b + c;
+            Console.Write((t / result) > k ? t / result : t / c);
+        }
+        public static void CalcMaxNumber54()
+        {
+            int t = 347;
+            int a = t / 100;
+            int b = (t / 10) % 10;
+            int c = t % 10;
+            if (a > b)
+            {
+                if (a > c)
+                    Console.Write("a is the largest!");
+                else
+                    Console.Write("c is the largest!");
+            }
+            else if (b > c)
+                Console.Write("b is the largest!");
+            else
+                Console.Write("c is the largest!");
+        }
+        public static void CalcMinNumber55()
+        {
+            int t = 347;
+            int a = t / 100;
+            int b = (t / 10) % 10;
+            int c = t % 10;
+            if (a < b)
+            {
+                if (a < c)
+                    Console.Write("a is the smallest!");
+                else
+                    Console.Write("c is the smallest!");
+            }
+            else if (b > c)
+                Console.Write("b is the smallest!");
+            else
+                Console.Write("c is the smallest!");
+        }
+        public static void Something56()
+        {
+            float t = 132;
+            float a = t / 100;
+            float b = (t / 10) % 10;
+            float c = t % 10;
+            float d = a + b + c;
+            float z = d / t;
+            Console.Write(c > b ? z : t);
         }
     }
 }
